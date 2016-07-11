2c10b0ab5c3210154f54b2b4da502e7b37e7897b
Aswathigupta <aswathi.g25@gmail.com>
2016-07-11 21:44:34 +0530

    Third commit

---
+++ src/com/niit/shoppingcart/LoginDAO.java
@@ -0,0 +1,19 @@
+package com.niit.shoppingcart;
+
+public class LoginDAO {
+	public boolean isValidUser(String userID, String password)
+
+	{
+
+		if (userID.equals(password))
+
+		{
+			return true;
+		}
+
+		else {
+			return false;
+		}
+	}
+
+}
